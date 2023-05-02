d9fbf4dccdba6f27ba6496e13d6e886c23bca254
Unyxe <114250878+Unyxe@users.noreply.github.com>
2023-05-02 16:15:22 +0100

    Add files via upload

--- AIWriter/AIWriter.cs
+++ AIWriter/AIWriter.cs
@@ -29,9 +29,11 @@ namespace AIWriter
                 return;
             }
             
-            string message = "<bard_search>\\search\\?auth-fb1f92fce9539c1a723adabc3e9eb875&query-" + query_txt_box.Text;
+            string message = "<bard_search>\\search\\?auth-fb1f92fce9539c1a723adabc3e9eb875&query-" + ToBase64(query_txt_box.Text);
             Console.WriteLine("[Request] " + message);
-            Send(message);
+            button1.Enabled = false;
+            Send(message, 10);
+            button1.Enabled = true;
             response_txt_box.Text = last_msg.Replace("\n", Environment.NewLine);
         }
 
@@ -57,40 +59,65 @@ namespace AIWriter
 
         static string binded_code = @"C:\binding.cs";
 
-        static string url = "http://unyxe.mywire.org:80";
+        static string url = "http://unyxe.mywire.org:8080";
 
 
         public static void TLSHandShake()
         {
             Console.WriteLine("[TLS handshake] Started...");
-            Send(tls_lib.GetPublicKey());
+            Send(tls_lib.GetPublicKey(), 10);
             symm_key = tls_lib.DecryptAssymetric(FromBase64ToByte(last_msg), tls_lib.GetPrivateKey());
             handshake_done = true;
             //Console.WriteLine(ToBase64FromByte(symm_key));
             Console.WriteLine("[TLS handshake] Success!");
         }
-        public static void Send(string str)
+        public static void Send(string str, int timeout)
         {
-            str += "~" + listening_port;
+            string d = str;
             string sent = ToBase64(str);
             if (handshake_done)
             {
                 sent = tls_lib.EncryptSymmetric(sent, symm_key);
             }
-            SendHttp(sent);
+            SendHttp(sent, timeout, d);
         }
-        public static void SendHttp(string str)
+        public static void Ping()
         {
-            HttpContent content = new StringContent(str);
+            while (true)
+            {
+                Send(@"<root>\log\", 1);
+            }
+
+        }
+        public static void SendHttp(string str, int timeout, string display_str)
+        {
+            HttpContent content = new StringContent(str + "~");
 
             HttpClient client = new HttpClient();
-            client.Timeout = TimeSpan.FromSeconds(10);
+            client.Timeout = TimeSpan.FromSeconds(timeout);
             client.DefaultRequestHeaders.Add("Connection_id", connection_id + "");
             DateTime now = DateTime.Now;
-            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            HttpResponseMessage response;
+            try
+            {
+                response = client.PostAsync(url, content).Result;
+            }
+            catch { Console.WriteLine("[Response] Failed to send, retry..."); SendHttp(str, timeout, display_str); return; }
             byte[] responseData = response.Content.ReadAsByteArrayAsync().Result;
             TimeSpan diff = DateTime.Now.Subtract(now);
             last_msg = Encoding.ASCII.GetString(responseData);
+            if (last_msg.StartsWith("Encryption failed"))
+            {
+                handshake_done = false;
+                Console.WriteLine("[TLS handshake] Key change...");
+                TLSHandShake();
+                Console.WriteLine("[TLS handshake] Key change finished!");
+
+
+                Console.WriteLine("[Request] " + display_str);
+                Send(display_str, timeout);
+                return;
+            }
             if (handshake_done)
             {
                 last_msg = tls_lib.DecryptSymmetric(last_msg, symm_key);
@@ -98,22 +125,6 @@ namespace AIWriter
                 Console.WriteLine(last_msg);
                 Console.Write("    Latency: " + diff.TotalMilliseconds + "ms \n");
             }
-        }
-        public static void SendToServer(string str)
-        {
-            Send(str + "~" + listening_port);
-        }
-        public static string GetLocalIPAddress()
-        {
-            var host = Dns.GetHostEntry(Dns.GetHostName());
-            foreach (var ip in host.AddressList)
-            {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    return ip.ToString();
-                }
-            }
-            return "255.255.255.255";
         }
 
         public static string ToBase64(string input)
